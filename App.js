f71a16bd50a7474bcc2f4611b27a386323c7de76
Logan-Rose <logan.rose0329@gmail.com>
2020-05-27 01:10:40 -0400

    set up weather code detecting and dictionary for weather types

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 
-let API_KEY = "1d55ff2f438304c43486d5eba9e29692"
-let city = "Ottawa"
-
+let WEATHER_KEY = "1d55ff2f438304c43486d5eba9e29692"
 
 
 //To parse APi weather codes into more general classes
@@ -16,6 +14,23 @@ let WEATHER_CODES = {
   8:"Clear"
 }
 
+let COLOR_CODES = {
+  2:"black",
+  3:"navy",
+  5:"navy",
+  6:"floralwhite",
+  7:"silver",
+  8:"lightskyblue"
+}  
+
+let MOOD_CODES = {
+  2:"rock",
+  3:"sad",
+  5:"lofi",
+  6:"rap",
+  7:"chill",
+  8:"pop"
+}
 
 class App extends React.Component {
   constructor() {
@@ -42,7 +57,7 @@ class App extends React.Component {
       error => Alert.alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
-    let url = "http://api.openweathermap.org/data/2.5/weather?lat=" + this.state.lat +"&lon=" + this.state.long+"&appid=" + API_KEY
+    let url = "http://api.openweathermap.org/data/2.5/weather?lat=" + this.state.lat +"&lon=" + this.state.long+"&appid=" + WEATHER_KEY
     fetch(url)
     .then(res => res.json())
     .then(json => this.setState({ weather: parseInt(json.weather[0].id.toString().charAt(0))}))
@@ -55,9 +70,17 @@ class App extends React.Component {
     console.log(this.state.long)
   }
 
+  mainStyle(){
+
+
+    return {
+      backgroundColor : COLOR_CODES[this.state.weather]
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, this.mainStyle()]}>
         <Text style = {styles.temperature}>{WEATHER_CODES[this.state.weather]}</Text>
         <Text style = {styles.temperature}>{this.state.loc}</Text>
         <Button onPress={() => this.getWeather()} title="fuck"></Button>
